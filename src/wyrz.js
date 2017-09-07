7f892a070163175cca6addae0deb4747260c88f8
Jacob Francis Powers <xxv9xx@gmail.com>
2017-09-07 06:52:18 -0500

    blacklist /socket.io from the express caller

--- src/wyrz.js
+++ src/wyrz.js
@@ -42,7 +42,7 @@ app.use(express.static('public'))
 httpd.on('request', (request, response) => {
   bot.incoming().call(httpd, request, response)
 
-  if (request.url.indexOf('/incoming') !== 0) {
+  if (request.url.indexOf('/incoming') !== 0 && request.url.indexOf('/socket.io') !== -1) {
     app.call(httpd, request, response)
   }
 })
