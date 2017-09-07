418fda45cca4f6adc20142ebbcf00cf3a526fae9
Jacob Francis Powers <xxv9xx@gmail.com>
2017-09-07 06:10:00 -0500

    disable forward

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-// require('./dist/wyrz')
+require('./dist/wyrz')
 
-const http = require('http')
-const express = require('express')
-
-let app = express()
-let httpd = http.Server(other)
-
-httpd.on('request', function (request, response) {
-  if (!response.finished) {
-    app.call(httpd, request, response)
-  }
-})
-
-app.use('/', (req, res) => {
-  res.end('express')
-})
-
-httpd.listen(8080)
-
-function other (request, response) {
-  console.log(request.url)
-  if (request.url.indexOf('/other') !== -1){
-    response.end('other')
-  }
-}
+// const http = require('http')
+// const express = require('express')
+//
+// let app = express()
+// let httpd = http.Server(other)
+//
+// httpd.on('request', function (request, response) {
+//   if (!response.finished) {
+//     app.call(httpd, request, response)
+//   }
+// })
+//
+// app.use('/', (req, res) => {
+//   res.end('express')
+// })
+//
+// httpd.listen(8080)
+//
+// function other (request, response) {
+//   console.log(request.url)
+//   if (request.url.indexOf('/other') !== -1){
+//     response.end('other')
+//   }
+// }
